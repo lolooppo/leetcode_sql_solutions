458124fb51004ad3db9ad5945a5ff3229ef197a0
Alaa_Omran <99070234+lolooppo@users.noreply.github.com>
2024-04-13 14:57:40 +0200

    Delete Sql 50 directory

--- Sql 50/p7.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT product_name, year, price
-
-FROM Sales
-
-INNER JOIN Product USING(product_id)
--- Sql 50/p9.sql
+++
@@ -1,7 +0,0 @@
-SELECT W1.id
-
-FROM Weather AS W1
-
-INNER JOIN Weather AS W2 ON DATEDIFF(W1.recordDate, W2.recordDate) = 1
-
-WHERE (W1.temperature > W2.temperature)
