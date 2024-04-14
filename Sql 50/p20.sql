d4d001c4a45e41ed993a140218cb0a2f8eefc924
Alaa_Omran <99070234+lolooppo@users.noreply.github.com>
2024-04-14 22:58:05 +0200

    Update p20.sql

--- Sql 50/p20.sql
+++ Sql 50/p20.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT SUBSTR(trans_date, 1, 7) month,country, COUNT(id) trans_count, SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) approved_count, sum(amount) trans_total_amount, sum(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_total_amount
+SELECT SUBSTR(trans_date, 1, 7) month, country, COUNT(id) trans_count, SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) approved_count, sum(amount) trans_total_amount, sum(CASE WHEN state = 'approved' THEN amount ELSE 0 END) approved_total_amount
 
 FROM Transactions
 
